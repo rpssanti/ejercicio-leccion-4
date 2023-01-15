228b7e89a3cf22eadfce4f4e5acad5fff0dfd2c7
Santi <121914283+rpssanti@users.noreply.github.com>
2023-01-15 03:37:40 +0100

    Add files via upload

---
+++ Ejercicio Leccion 4.js
@@ -0,0 +1,23 @@
+//Ejercicio lección 4 Cadenas de Texto
+
+let cadena = "Santy"
+
+let cadena2 = "Pérez"
+
+let estudiante = "Santy Pérez"
+
+let estudianteMayus = "SANTY PÉREZ"
+
+let estudianteMinus = "santy pérez"
+
+console.log(estudiante.length);
+
+console.log(cadena.charAt(0))
+
+console.log(cadena2.charAt(4))
+
+let cadena4 = "SantyPérez"
+
+console.log(estudiante.includes("Santy"))
+
+
